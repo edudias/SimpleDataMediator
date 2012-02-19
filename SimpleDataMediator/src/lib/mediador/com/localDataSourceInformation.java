a4aebf2e043cc0e40751eb66acfce1d759488af3
edudias <edudias@me.com>
2012-02-19 16:16:11 -0200

    projeto do mediador

---
+++ SimpleDataMediator/src/lib/mediador/com/localDataSourceInformation.java
@@ -0,0 +1,55 @@
+package lib.mediador.com;
+
+public class localDataSourceInformation {
+	private String serverName;
+	private String userName;
+	private String password;
+	private String connectionString;
+	private Integer timeout;
+	
+	public localDataSourceInformation(){
+		
+	}
+
+	public String getServerName() {
+		return serverName;
+	}
+
+	public void setServerName(String serverName) {
+		this.serverName = serverName;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getConnectionString() {
+		return connectionString;
+	}
+
+	public void setConnectionString(String connectionString) {
+		this.connectionString = connectionString;
+	}
+
+	public Integer getTimeout() {
+		return timeout;
+	}
+
+	public void setTimeout(Integer timeout) {
+		this.timeout = timeout;
+	}
+	
+	
+}
---
+++ SimpleDataMediator/src/reescrita/mediador/com/reescrita.java
@@ -0,0 +1,10 @@
+package reescrita.mediador.com;
+
+
+import lib.mediador.com.*;
+
+public class reescrita {
+
+	ecg ECG = new ecg();
+	
+}
