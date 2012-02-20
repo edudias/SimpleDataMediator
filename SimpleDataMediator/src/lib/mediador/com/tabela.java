864da9dac23d15a2025080d89a472eff62beb927
edudias <edudias@me.com>
2012-02-20 19:10:37 -0200

    problema no usa da branch-reescrita

---
+++ SimpleDataMediator/src/lib/mediador/com/tabela.java
@@ -0,0 +1,50 @@
+package lib.mediador.com;
+
+import java.util.List;
+
+public class tabela {
+
+	private String nomeTabela;
+	private List<campoTabela> camposTabela;
+	private String viewGAV;
+	private String viewLAV;
+	
+	public tabela(){
+		
+	}
+	
+	public String getNomeTabela() {
+		return nomeTabela;
+	}
+	
+	public void setNomeTabela(String nomeTabela) {
+		this.nomeTabela = nomeTabela;
+	}
+	
+	public List<campoTabela> getCamposTabela() {
+		return camposTabela;
+	}
+	
+	public void setCamposTabela(List<campoTabela> camposTabela) {
+		this.camposTabela = camposTabela;
+	}
+
+	public String getViewGAV() {
+		return viewGAV;
+	}
+
+	public void setViewGAV(String viewGAV) {
+		this.viewGAV = viewGAV;
+	}
+
+	public String getViewLAV() {
+		return viewLAV;
+	}
+
+	public void setViewLAV(String viewLAV) {
+		this.viewLAV = viewLAV;
+	}
+	
+	
+	
+}
---
+++ SimpleDataMediator/src/sqlparse/mediador/com/sqlparse.java
@@ -0,0 +1,5 @@
+package sqlparse.mediador.com;
+
+public class sqlparse {
+
+}
